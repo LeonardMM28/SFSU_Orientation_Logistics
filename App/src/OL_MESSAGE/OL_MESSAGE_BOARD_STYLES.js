7d86e3446c94026e6235b99a3bc2e915e63d2d50
Leonardo Meza Martinez <leonardomezamartinez@gmail.com>
2024-07-18 12:59:40 -0700

    Refactor OL_MESSAGE_BOARD to remove EscapeButton component and update styling

--- App/src/OL_MESSAGE/OL_MESSAGE_BOARD_STYLES.js
+++ App/src/OL_MESSAGE/OL_MESSAGE_BOARD_STYLES.js
@@ -509,25 +509,6 @@ const TimeText = styled.div`
 `;
 
 
-export const EscapeButton = styled.button`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: #e74c3c;
-  color: white;
-  border: none;
-  padding: 10px;
-  cursor: pointer;
-  font-size: 16px;
-  border-radius: 5px;
-  transition: background-color 0.3s;
-
-  &:hover {
-    background-color: #c0392b;
-  }
-`;
-
 export const OLPowerContainer = styled.div`
   display: flex;
   align-items: center;
